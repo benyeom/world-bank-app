e9d8b17af31b3c37219be2d7a40d7897a53af344
benjaminyeom <benyeom042@gmail.com>
2025-02-19 13:18:47 -0800

    Initial commit.

---
+++ our_indicator.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from pandas_datareader import wb
+
+df = wb.get_indicators()[['id','name']]
+df = df[df.name == 'Individuals using the Internet (% of population)']
+print(df)
+
+print("hello")
